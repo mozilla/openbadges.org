08b6075ad56078b38eee6f94b6a79b9675412cbe
Andrew Hayward <github@andrewhayward.net>
2013-03-05 11:11:44 +0000

    404 page (plus fixes)

---
+++ wordpress/wp-content/themes/openbadges/404.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+<article>
+	<div class="content">
+		<h2>Page not found</h2>
+		<div class="inner">
+			<p>Unfortunately, we can't find the page you're looking for.</p>
+		</div>
+	</div>
+</article>
+
+<?php get_footer(); ?>
